f2a2f47727cc514bcc45552ab06b14b66ac83278
MrTrojan7 <yur4ik.karataev@gmail.com>
2021-12-06 20:36:13 +0200

    begin lesson 2

--- main.cpp
+++ main.cpp
@@ -1,33 +1,9 @@
 #include <iostream>
-#include "Stack.h"
-#include "Computer.h"
-#include "Book.h"
+
 
 using namespace std;
 
 int main()
 {
-	srand(time(0));
-	/*Stack stack;
-	for (int i = 0; i < 5; i++)
-	{
-		stack.Push(i + 1);
-	}
-    
-	while (!stack.IsEmpty())
-	{
-		cout << stack.Pop() << endl;
-	}*/
-
-	/*Computer comp1;
-	comp1.Show();
-	Computer comp2("Dell", "Linux", "AMD", 10);
-	comp2.Show();*/
-
-	/*Book a;
-	a.Show();
-	Book b("D.K.Rouling", "Harry Potter", "Fantastic", "England", 700, 1, 2005);
-	b.Show();*/
-
-
+	return 0;
 }
