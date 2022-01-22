28513503630f2fa0ee669f83eac3da45572638d5
MrTrojan7 <yur4ik.karataev@gmail.com>
2022-01-22 23:47:35 +0200

    Vector is begin

---
+++ Vector.h
@@ -0,0 +1,13 @@
+#pragma once
+
+template <class T>
+
+class Vector
+{
+private:
+	T* collection;
+	unsigned int size;
+public:
+
+};
+
--- main.cpp
+++ main.cpp
@@ -4,34 +4,6 @@
 int main()
 {
 	srand(time(NULL));
-	int size = 100;
-	int* arr = new int[size];
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = rand() % 200 + (-100);// / 1.1; test with <double>
-	}
-	Sorter<int> test;
-	test.PrintArray(arr, size);
-	test.qsortRecursive(arr, size);
-	test.PrintArray(arr, size);
-	/*for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << "\t";
-		if ((i + 1) % 10 == 0)
-		{
-			cout << endl;
-		}
-	}*/
-	
-	/*for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << "\t";
-		if ((i + 1) % 10 == 0)
-		{
-			cout << endl;
-		}
-	}*/
-	delete[] arr;
 
 	return 0;
 }
